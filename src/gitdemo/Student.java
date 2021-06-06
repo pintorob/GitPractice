c855c8cbf7c59639f6d1b9ce961f9395711615fd
Robert Pinto <pintorob4@gmail.com>
2021-06-06 15:31:22 -0400

    student file added

---
+++ src/gitdemo/Student.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gitdemo;
+
+/**
+ *
+ * @author Robert Moses Pinto
+ */
+public class Student {
+    private String name;
+    private int id;
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+   
+
+}
